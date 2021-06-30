7df2543628192b63b2d9c5274bb43a71f6f95080
Jesus Alan Cristhoffer Martinez Santillan <jalanmartinez@gmail.com>
2021-06-30 13:20:49 -0700

    base

---
+++ content/js/home.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    var previousScroll = 0,
+        $messageSection = $(".message_section"),
+        $titleBar = $(".title_bar");
+    $messageSection.scroll(function() {
+        var currentScroll = $messageSection.scrollTop();
+        if (currentScroll > previousScroll) {
+            if (currentScroll > 0 & currentScroll <= 80) {
+                $titleBar.css("margin-top", `-${currentScroll}px`);
+            } else {
+                $titleBar.css("margin-top", `-80px`);
+            }
+        } else {
+            if (currentScroll <= 90) {
+                $titleBar.css("margin-top", `-${currentScroll}px`);
+            } else {
+                $titleBar.css("margin-top", `-80px`);
+            }
+        }
+        previousScroll = currentScroll;
+    });
+
+
+});
